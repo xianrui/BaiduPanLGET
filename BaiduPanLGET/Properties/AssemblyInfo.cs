ac59d6af721832fe78f3b3bb93b72d9e4f07010b
hang333 <jonathan.goh333@gmail.com>
2015-03-29 17:28:48 +0800

    Edit something.

--- BaiduPanLGET/Properties/AssemblyInfo.cs
+++ BaiduPanLGET/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BaiduPanLGET")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  hang333 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
